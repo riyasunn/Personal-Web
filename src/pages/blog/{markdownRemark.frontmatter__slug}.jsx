5f11eab94293a469397226cdeb6b03e2dbcc37cb
Riya Sun <riya.sunn@gmail.com>
2023-03-11 17:35:02 +1300

    blog template: styled individual blog template page

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -1,38 +1,79 @@
-import * as React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
 import { Layout } from '@components';
+import { Helmet } from 'react-helmet';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+import PropTypes from 'prop-types';
 
-export default function BlogPostTemplate({
-  location,
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
+const StyledHeader = styled.header`
+  text-align: center;
+  h1 {
+    font-size: var(--fz-heading);
+  }
+  h2 {
+    margin: 30px auto 30px auto;
+    color: var(--black);
+    font-size: var(--fz-m);
+    font-weight: normal;
+  }
+  
+`;
+
+const StyledBlogContainer = styled.div`
+  line-height: 50px;
+  margin: 0 20px;
+  text-align: justify;
+`;
+
+const BlogPostTemplate = ({ location, data }) => {
+  const { markdownRemark } = data; 
   const { frontmatter, html } = markdownRemark;
+
+  const revealTitle = useRef(null);
+  const revealBlog = useRef([]);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealTitle.current, srConfig());
+    sr.reveal(revealBlog.current, srConfig(200, 0));
+  }, []);
+
   return (
-    // <div>
-    //   <div>
-    //     <h1>{frontmatter.title}</h1>
-    //     <h2>{frontmatter.date}</h2>
-    //     <div dangerouslySetInnerHTML={{ __html: html }} />
-    //   </div>
-    // </div>
     <Layout location={location}>
-      <div>
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </div>
+      <Helmet title="Blogs" />
+      
+      <main>
+        <StyledHeader ref={revealTitle}>
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+        </StyledHeader>
+
+        <StyledBlogContainer ref={revealBlog}>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </StyledBlogContainer>
+      </main>
     </Layout>
   );
-}
+};
+BlogPostTemplate.propTypes = {
+  data: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+};
+
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
-        # date(formatString: "MMMM DD, YYYY")
         date
         slug
         title
