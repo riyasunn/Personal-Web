352f1d6553a5c8e42a3209700e2ac0400779b879
Riya Sun <riya.sunn@gmail.com>
2023-03-11 16:09:52 +1300

    blog: basic layout for individual page works

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import { Layout } from '@components';
 
 export default function BlogPostTemplate({
+  location,
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
   return (
-    <div>
+    // <div>
+    //   <div>
+    //     <h1>{frontmatter.title}</h1>
+    //     <h2>{frontmatter.date}</h2>
+    //     <div dangerouslySetInnerHTML={{ __html: html }} />
+    //   </div>
+    // </div>
+    <Layout location={location}>
       <div>
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </div>
-    </div>
+    </Layout>
   );
 }
 
